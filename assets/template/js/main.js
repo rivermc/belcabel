bf85f8b424f1f8a8995342ee0e49e4e7da97e99d
AlexeyPakhomov <web@2f-vsk.ru>
2019-11-27 21:50:08 +0100

    brands card

--- assets/template/js/main.js
+++ assets/template/js/main.js
@@ -17,6 +17,31 @@ function getModule(action, module, params, cb, delimiter = ',') {
 }
 
 
+$(document).ready(function() {
 
+    var $content = $('.js_content');
+    var content_height = $content.height();
+    var content_min_height = '25em';
+
+    if (content_height >= 300) {
+        $content.height(content_min_height);
+        var more_link = $('<a href="#" class="js_no_preload js_more">Читать полностью</a>');
+        more_link.on('click', function(e) {
+            e.preventDefault();
+            var $el = $(e.currentTarget);
+            if ($el.hasClass('js_active')) {
+                $content.height(content_min_height);
+                $el.removeClass('js_active').text('Читать полностью');
+            }
+            else {
+                $content.height(content_height + 10);
+                $el.addClass('js_active').text('Свернуть');
+            }
+        });
+
+        $content.after(more_link);
+    }
+
+});
 
   
