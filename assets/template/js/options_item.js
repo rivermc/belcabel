6174f9d1ede01266af8a29153c5ff75436b3dad3
AlexeyPakhomov <web@2f-vsk.ru>
2019-12-01 15:38:55 +0100

    catalog

--- assets/template/js/options_item.js
+++ assets/template/js/options_item.js
@@ -1,14 +1,9 @@
 function OptionsItem() {
-  $('.js_options_button').click(function() {
-    var product_id = $(this).data('id');
-    var product_title = $(this).data('title');
-    getModule('Chunk', 'OptionsItem', 'id => ' + product_id,function (data) {
-      var close_button = $('<div class="close_button mfp-close"></div>');
-      var title = '<h2 class="h1 page_title">Характеристики <span class="sub">'+ product_title +'</span></h2>';
-      var properties_wrap = $('<div class="properties_wrap">').append(title).append(data);
-      var data_import = $(close_button).add(properties_wrap);
-      $('.js_options_target').html(data_import);
-    });
+  $('.js_options_button').click(function(e) {
+    $('.js_options_target').removeClass('js_active');
+    e.preventDefault();
+    var $target = $(this).parent().parent().find('.js_options_target');
+    $target.toggleClass('js_active');
   });
 }
 
---
+++ assets/template/js/tabs.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+  var $content = $('.js_content');
+  var content_height = $content.height();
+  var content_min_height = '25em';
+
+  if (content_height >= 300) {
+    $content.height(content_min_height);
+    var more_link = $('<a href="#" class="js_no_preload js_more">Читать полностью</a>');
+    more_link.on('click', function(e) {
+      e.preventDefault();
+      var $el = $(e.currentTarget);
+      if ($el.hasClass('js_active')) {
+        $content.height(content_min_height);
+        $el.removeClass('js_active').text('Читать полностью');
+      }
+      else {
+        $content.height(content_height + 10);
+        $el.addClass('js_active').text('Свернуть');
+      }
+    });
+
+    $content.after(more_link);
+  }
+
+  $('.js_tabs_menu').click( function() {
+    var tab_id = $(this).attr('data-id');
+    $(this).addClass('js_active').siblings().toggleClass('js_active', false);
+    $('.js_tabs_item[data-id='+ tab_id + ']').toggleClass('js_active', true).siblings().toggleClass('js_active', false);
+  });
+
+
+
+});
