aa802144dcdc8de89ccf573d71e65394414e1b4a
AlexeyPakhomov <web@2f-vsk.ru>
2019-12-12 21:28:58 +0100

    optimization loads

---
+++ assets/template/js/lazyload_css.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+  let head = window.document.getElementsByTagName('head')[0];
+  function includeCSS(aFile, aRel) {
+    let style = window.document.createElement('link');
+    style.href = aFile;
+    style.rel = aRel || 'stylesheet';
+    head.appendChild(style);
+  }
+
+  includeCSS('/assets/template/css/index.css');
+  includeCSS('/assets/template/css/magnific-popup.css');
+  includeCSS('/assets/template/css/minishop.css');
+
+});
